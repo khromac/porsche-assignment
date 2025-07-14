190cfe0ead17eb080475b4f7e098205da0c67b96
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-14 21:05:54 +0200

    remove prebuilt content

---
+++ app/pages/homePage/HomePage.tsx
@@ -0,0 +1,8 @@
+
+export function HomePage() {
+  return (
+    <main className="flex items-center justify-center pt-16 pb-4">
+
+    </main>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { HomePage } from "~/pages/homePage/HomePage";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Porsche - Home" },
+    { name: "Home", content: "Porsche home page" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <HomePage />;
 }
