6b4512df8c2777f0e8156aba8dd577abc5facccf
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-14 22:00:23 +0200

    fresh project and starting navigation bar

--- app/pages/homePage/HomePage.tsx
+++ app/pages/homePage/HomePage.tsx
@@ -1,8 +1,16 @@
+import porscheLogo from "../../assets/porsche-logo.webp"
 
 export function HomePage() {
-  return (
-    <main className="flex items-center justify-center pt-16 pb-4">
+    return (
+        <>
+            <nav className="grid grid-cols-3 border py-4 px-5">
+                <h3 className="">Menu</h3>
+                <img src={porscheLogo} alt="porsche logo" width="200px" className="justify-self-center"/>
+                <h3 className="justify-self-end">Porsche Centar Zagreb</h3>
+            </nav>
+            <main className="flex items-center justify-center pt-16 pb-4">
 
-    </main>
-  );
+            </main>
+        </>
+    );
 }
