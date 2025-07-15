ae2f74f7276fdf4b3474415b3a3e0391f54d8dbf
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-16 00:30:28 +0200

    added footer component

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -3,9 +3,9 @@ import porscheLogo from "~/assets/images/porsche-logo.svg";
 const NavBar = () => {
     return (
         <nav className="grid grid-cols-3 items-center py-4 px-5">
-            <h3 className="">Menu</h3>
+            <p className="">Menu</p>
             <img src={porscheLogo} alt="porsche logo" width="200px" className="justify-self-center"/>
-            <h3 className="justify-self-end">Porsche Centar Zagreb</h3>
+            <p className="justify-self-end">Porsche Centar Zagreb</p>
         </nav>
     );
 }
