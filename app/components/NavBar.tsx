14886fb534191f59731b00283328e80b10f42023
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-15 23:36:39 +0200

    added porsche-logo.svg and replaced the old one

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,8 +1,8 @@
-import porscheLogo from "~/assets/images/porsche-logo.webp";
+import porscheLogo from "~/assets/images/porsche-logo.svg";
 
 const NavBar = () => {
     return (
-        <nav className="grid grid-cols-3 py-4 px-5">
+        <nav className="grid grid-cols-3 items-center py-4 px-5">
             <h3 className="">Menu</h3>
             <img src={porscheLogo} alt="porsche logo" width="200px" className="justify-self-center"/>
             <h3 className="justify-self-end">Porsche Centar Zagreb</h3>
