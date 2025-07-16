7ed8e265fb813a226d9853a201e084e8f03ba9d6
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-16 21:44:27 +0200

    homepage refresh and used car data from file

--- app/pages/homePage/HomePage.tsx
+++ app/pages/homePage/HomePage.tsx
@@ -1,88 +1,60 @@
 import Hero1 from "~/assets/images/taycan-hero.jpg";
-import Porsche911 from "~/assets/images/porsche-model-911.jpg";
-import PorscheTaycan from "~/assets/images/porsche-model-taycan.jpeg";
-import Porsche718 from "~/assets/images/porsche-model-718.jpeg";
 import Card from "~/components/Card";
 import Select from "~/components/Select";
-import type {CardProps, EngineModel} from "~/types";
-import {useState} from "react";
-
+import { useEffect, useState } from "react";
+import { carModels, engineModels } from "~/data/carsData";
+import type { CardProps } from "~/types";
 
 export function HomePage() {
     const [filter, setFilter] = useState<string>("all");
-    const engineModels: Record<EngineModel, string> = {
-        all: "All",
-        electric: "Electric",
-        hybrid: "Hybrid",
-        petrol: "Petrol",
-    }
+    const [filteredCars, setFilteredCars] = useState<CardProps[]>([]);
 
-    const carModels: Record<string, CardProps> = {
-        'porsche-911': {
-            name: 'Porsche 911',
-            description: 'Iconic sports car with rear engine: 2 doors',
-            image: Porsche911,
-            engineType: "hybrid"
-        },
-        'porsche-taycan': {
-            name: 'Porsche Taycan',
-            description: 'Electric sports car: 4 doors',
-            image: PorscheTaycan,
-            engineType: "petrol"
-        },
-        'porsche-718': {
-            name: 'Porsche 718',
-            description: 'Precise mid-engine sports car',
-            image: Porsche718,
-            engineType: "electric"
-        },
-        'porsche-719': {
-            name: 'Porsche 748',
-            description: 'Precise mid-engine sports car with a',
-            image: Porsche718,
-            engineType: "petrol"
-        },
-    }
+    useEffect(() => {
+        const carsArray = Object.values(carModels);
+        if (filter === "all") {
+            setFilteredCars(carsArray);
+        } else {
+            const filtered = carsArray.filter(car => car.engineType === filter);
+            setFilteredCars(filtered);
+        }
+    }, [filter]);
 
     return (
-        <>
-            <main className="flex items-center justify-center flex-col gap-4">
-                {/*HERO SECTION*/}
-                <section className="flex flex-col justify-center w-full">
-                    <div className="relative flex">
-                        <img src={Hero1} alt="porsche dealership" className="w-full h-auto object-cover shadow-lg"/>
-                        <div
-                            className="absolute inset-x-0 bottom-0 top-1/2 bg-gradient-to-b from-transparent to-black/60 pointer-events-none"/>
-                        <div
-                            className="absolute bottom-[10%] left-[10%] flex flex-col items-start justify-end font-bold text-white">
-                            <h1 className="text-4xl md:text-5xl font-medium">More than a feeling</h1>
-                            <p className="text-2xl font-light">Welcome.</p>
-                        </div>
+        <main className="flex items-center justify-center flex-col gap-4">
+            {/* HERO SECTION */}
+            <section className="flex flex-col justify-center w-full">
+                <div className="relative flex">
+                    <img src={Hero1} alt="porsche dealership" className="w-full h-[98dvh] object-cover shadow-lg" />
+                    <div className="absolute inset-x-0 bottom-0 top-1/2 bg-gradient-to-b from-transparent to-black/60 pointer-events-none" />
+                    <div className="absolute bottom-[10%] left-[10%] flex flex-col items-start justify-end font-bold text-white">
+                        <h1 className="text-4xl md:text-5xl font-medium">More than a feeling</h1>
+                        <p className="text-2xl font-light">Welcome.</p>
                     </div>
-                </section>
+                </div>
+            </section>
 
-                <section className="flex flex-col justify-center w-full px-8 md:px-16 gap-10 mt-4 my-10">
-                    <div className="grid grid-cols-2">
-                        <h1 className="text-2xl font-bold">Available models</h1>
-                        <div className="flex flex-row justify-self-end">
-                            <h1 className="text-2xl font-bold ">Filter:</h1>
-                            <Select options={engineModels} onChange={setFilter} value={filter}/>
-                        </div>
+            <section className="flex flex-col justify-center w-full px-8 md:px-16 gap-10 mt-4 my-10">
+                <h1 className="text-2xl font-medium text-left my-10 md:my-20 md:text-4xl md:text-center lg:text-7xl">
+                    Where Your journey begins.
+                </h1>
 
-                    </div>
-                    <div className="group flex flex-col justify-center items-center gap-5 overflow-x-auto lg:flex-row lg:flex-wrap">
-                        {Object.entries(carModels).map(([key, model]) => (
-                                <Card
-                                    name={model.name}
-                                    description={model.description}
-                                    image={model.image}
-                                    engineType={model.engineType}
-                                    key={key}
-                                />
-                        ))}
-                    </div>
-                </section>
-            </main>
-        </>
+                <div className="flex flex-row items-center justify-end gap-4">
+                    <p>Filter:</p>
+                    <Select options={engineModels} onChange={setFilter} value={filter} />
+                </div>
+
+                <div className="group flex flex-col justify-center items-center gap-5 overflow-x-auto lg:flex-row lg:flex-wrap">
+                    {filteredCars.map((model, index) => (
+                        <Card
+                            key={index}
+                            name={model.name}
+                            description={model.description}
+                            image={model.image}
+                            engineType={model.engineType}
+                        />
+                    ))}
+                </div>
+            </section>
+        </main>
     );
 }
