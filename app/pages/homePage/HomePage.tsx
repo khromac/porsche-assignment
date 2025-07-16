0addacfec56e915847953f60a952eb202cefe90f
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-17 01:30:36 +0200

    homepage and badge TW refactoring

--- app/pages/homePage/HomePage.tsx
+++ app/pages/homePage/HomePage.tsx
@@ -42,22 +42,21 @@ export function HomePage() {
 
     return (
         <main className="flex items-center justify-center flex-col gap-4">
-            {/* HERO SECTION */}
             <section className="flex flex-col justify-center w-full">
                 <div className="relative flex">
                     <img src={Hero1} alt="porsche dealership" className="w-full h-[98dvh] object-cover shadow-lg" />
-                    <div className="absolute inset-x-0 bottom-0 top-1/2 bg-gradient-to-b from-transparent to-black/60 pointer-events-none" />
-                    <div className="absolute bottom-[10%] left-[10%] flex flex-col items-start justify-end font-bold text-white">
-                        <h1 className="text-4xl md:text-5xl font-medium lg:text-7xl">More than a feeling</h1>
+                    <div className="hero-overlay" />
+                    <div className="hero-text">
+                        <h1 className="text-4xl font-medium md:text-5xl lg:text-7xl">More than a feeling</h1>
                         <p className="text-2xl font-light">Welcome.</p>
                     </div>
                 </div>
             </section>
 
-            <section className="flex flex-col justify-center w-full px-8 md:px-16 gap-10 mt-4 my-10">
-                <h1 className="text-2xl font-medium text-left my-10 md:my-20 md:text-4xl md:text-center lg:text-7xl">
+            <section className="flex flex-col justify-center w-full px-8 gap-10 mt-4 my-10 md:px-16 ">
+                <h2 className="text-left my-10 md:my-20 md:text-4xl md:text-center lg:text-7xl">
                     Where Your journey begins.
-                </h1>
+                </h2>
 
                 <div className="flex flex-row items-center justify-between gap-4">
                     <div className="flex flex-row items-center gap-4">
