40a8333a08f016b2bfe8ea57922dd4565bbbbfbe
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-16 12:29:04 +0200

    import fa icons

--- app/root.tsx
+++ app/root.tsx
@@ -23,6 +23,13 @@ export const links: Route.LinksFunction = () => [
     rel: "stylesheet",
     href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
   },
+    {
+        rel: "stylesheet",
+        href: "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css",
+        integrity: "sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==",
+        crossOrigin: "anonymous",
+        referrerPolicy: "no-referrer",
+    },
 ];
 
 export function Layout({ children }: { children: React.ReactNode }) {
