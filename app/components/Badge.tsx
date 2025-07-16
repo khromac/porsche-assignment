8bcdde2b88ce626c9a15626bcb0aabe4ab88cf3d
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-16 14:35:32 +0200

    badge visual tweak

--- app/components/Badge.tsx
+++ app/components/Badge.tsx
@@ -2,7 +2,7 @@ import type {BadgeProps} from "~/types";
 
 const Badge = ({ value }: BadgeProps) => {
     return (
-        <div className="badge px-3 py-1 rounded-full bg-gray-200 text-green-800 text-xs font-semibold uppercase w-fit">
+        <div className="badge px-3 py-1 rounded-lg bg-gray-200 text-neutral-800 text-xs font-semibold uppercase w-fit">
             {value}
         </div>
     );
