c72845866d2a77f18f1071d482ae7b05d1ba72a7
Mihael Smrcek <25547789+khromac@users.noreply.github.com>
2025-07-17 13:00:51 +0200

    footer - TW refactor

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -38,9 +38,9 @@ const Footer = () => {
 
     }
     return (
-        <footer className="bg-black text-white flex flex-col gap-10 p-10 pb-0">
+        <footer className="footer-container">
 
-            <div className="flex flex-col gap-3">
+            <div className="footer-section">
                 <h2>Current Region / Language</h2>
                 <div className="flex flex-row gap-10" >
                     <div className="flex flex-row gap-3">
@@ -52,13 +52,13 @@ const Footer = () => {
             </div>
 
             <div className="flex flex-col lg:flex-row gap-10">
-                <div className="flex flex-col gap-3 ">
+                <div className="footer-section">
                     <h2>Locations & Contacts</h2>
                     <p>Do you have any questions?</p>
                     <Button type="secondary" className="w-full mt-3 h-14"/>
                 </div>
 
-                <div className="flex flex-col gap-3 ">
+                <div className="footer-section">
                     <h2>Social Media</h2>
                     <p>Get in touch with us via social media.</p>
                     <div className="flex flex-wrap gap-2 w-70 mt-3">
@@ -76,7 +76,7 @@ const Footer = () => {
             </div>
 
 
-            <div className="flex flex-col gap-3 ">
+            <div className="footer-section">
                 <h2>Company</h2>
                 <div className="flex flex-wrap gap-10 w-full">
                     {companyItems.map((item, index) => (
@@ -88,7 +88,7 @@ const Footer = () => {
             <hr className="my-10 text-neutral-600" />
 
 
-            <div className="flex flex-col gap-5">
+            <div className="footer-section">
                 <p>© 2025 Dr. Ing. h.c. F. Porsche AG</p>
                 <p>
                     * If the values are given as ranges, these do not relate to a single, individual
@@ -108,7 +108,7 @@ const Footer = () => {
                 </p>
             </div>
 
-            <div className="flex w-full justify-center items-center p-3 my-15">
+            <div className="footer-logo-container">
                <img src={PorscheLogo} alt="porsche logo" width="250rem" style={{ filter: "invert(1)" }} />
             </div>
 
